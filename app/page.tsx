1babf54bf3fda6e07276785cf29c6a6ca55dc730
sicktastic <antwonlee@gmail.com>
2025-02-26 22:14:11 -0600

    chore: Add slap happy youtube link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
-import SlapHappy from "@/public/slap-happy.png";
+import Link from "next/link";
+import SlapHappyLogo from "@/public/slap-happy.png";
+import SlapHappy from "@/public/slap-happy.jpg";
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex h-screen justify-center items-center gap-4 sm:flex-row flex-col">
       <div>
-        <Image src={SlapHappy} alt="Slap Happy" className="h-80 w-80 " />
+        <Image
+          src={SlapHappyLogo}
+          alt="Slap Happy"
+          className="h-40 w-40 border rounded-xl"
+        />
+      </div>
+      <div>
+        <Link href="https://www.youtube.com/watch?v=m5PwVNDSY60&list=PLnTcKOMw1qGphoZjXdNacrd3ZYU95FYKe">
+          <Image
+            src={SlapHappy}
+            alt="Slap Happy"
+            className="w-56 rounded-xl border h-40 hover:border-teal-700"
+          />
+        </Link>
       </div>
     </div>
   );
